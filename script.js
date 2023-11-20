2a5c6d917b639793b02b609a50a74f71c44fdd57
Julian Betts <wjulianbetts@gmail.com>
2023-11-19 18:25:04 -0800

    greg totally fucking ruined it

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-
+// reseach change event event listener for dropdown of notes in chord 
 // Initial setup
+const fretboard = document.createElement('table');
 var standardTuning = Array( 8, 3, 11, 6, 1, 8);
-var fingering = [1, 3, 3, 3, 1, 0];
 
 const chromaticNotes = [
     {name: 'a♭', value: 12},
@@ -25,27 +25,40 @@ const chromaticNotes = [
 
 // Window onload event
 window.onload = () => {
-    setupChordBank();
-    renderFretboard(standardTuning, fingering);
+    renderChordBank();
+    renderFretboard(standardTuning, []);
 };
 
 // Render fretboard function
-const renderFretboard = (tuning, fingering) => {
-    const fretboard = document.createElement('table');
+// const renderFretboard = (tuning, fingering) => {
+//     const fretboard = document.createElement('table');
+//     // Loop through 6 strings
+//     for (let stringNumber = 0; stringNumber < 6; stringNumber++) {
+//         renderString(fretboard, stringNumber);
+//     }
+//     document.body.appendChild(fretboard);
+// };
+
+const renderFretboard = (tuning, selectedNotes) => {
+    fretboard.innerHTML = ''
+    // Add fret numbers row
+    const fretNumbersRow = document.createElement('tr');
+    fretNumbersRow.appendChild(document.createElement('th')); // Empty cell for the bottom 
     // Loop through 6 strings
     for (let stringNumber = 0; stringNumber < 6; stringNumber++) {
-        renderString(fretboard, stringNumber);
+        renderString(fretboard, stringNumber, selectedNotes);
     }
+    //extracted function
+    renderFretNumbers(fretNumbersRow, fretboard);
     document.body.appendChild(fretboard);
 };
 
+
 // Render string function
-const renderString = (fretboard, stringNumber) => {
+const renderString = (fretboard, stringNumber, selectedNotes) => {
     const string = fretboard.appendChild(document.createElement('tr'));
-    //TODO The default selected option is chosen from standard tuning.
+    var currentNote = standardTuning[stringNumber]
     string.appendChild(document.createElement('th')).appendChild(createChromaticDropdown(standardTuning[stringNumber]));
-
-
     // Loop through 12 frets for the current string
     for (let fretNumber = 0; fretNumber < 12; fretNumber++) {
         const fret = string.appendChild(document.createElement('td'));
@@ -53,53 +66,62 @@ const renderString = (fretboard, stringNumber) => {
         if (fretNumber === 0) {
             renderNut(fretNumber, fret, stringNumber);
         } else {
-            renderFret(fretNumber, fret, stringNumber);
+            renderFret(fret, stringNumber, currentNote, selectedNotes);
+            currentNote++
         }
     }
 };
 
 // Render nut function
 const renderNut = (fretNumber, fret, stringNumber) =>  {
-    if(fretNumber === fingering[stringNumber]) {
-        fret.innerHTML = 'x';
-    } else {
+//    if(fretNumber === fingering[stringNumber]) {
+//        fret.innerHTML = 'x';
+//    } else {
         fret.innerHTML = '|';
-    }
+//    }
 };
 
 // Render fret function
-const renderFret = (fretNumber, fret, stringNumber) => {
+const renderFret = (fret, stringNumber, currentNote, selectedNotes) => {
     // Check if this fret is fingered in the current chord
-    if(fretNumber === fingering[stringNumber]) {
+    if(selectedNotes.indexOf(currentNote) > -1) {
         fret.innerHTML = 'X';
     } else {
        fret.innerHTML = '—';
     }
 };
 
-const setupChordBank = () => {
+const renderChordBank = () => {
     const chordBankFormEl = document.createElement('form')
-
+    chordBankFormEl.id = 'chordBank'
+    chordBankFormEl.addEventListener('change', (e) => {
+        updateSelectedNotes()
+    })
     for (let i = 0; i < 12; i++) {
 
         const chordNote = createChromaticDropdown();
 
         chordBankFormEl.appendChild(chordNote);
     }
-
-    const submitBtn = document.createElement('button')
-    chordBankFormEl.appendChild(submitBtn)
     document.body.appendChild(chordBankFormEl);
-
 };
 
+function renderFretNumbers(fretNumbersRow, fretboard) {
+    for (let fretNumber = 0; fretNumber < 12; fretNumber++) {
+        const fretNumberCell = document.createElement('th');
+        fretNumberCell.innerText = (fretNumber).toString();
+        fretNumbersRow.appendChild(fretNumberCell);
+        fretNumbersRow[fretNumber];
+    }
+    fretboard.appendChild(fretNumbersRow);
+}
 
 function createChromaticDropdown(tuning) {
     const chordNote = document.createElement('select');
     const emptyOptionEl = document.createElement('option');
     chordNote.appendChild(emptyOptionEl);
     for (let j = 0; j < chromaticNotes.length; j++) {
-        console.log(chromaticNotes[j]);
+        // console.log(chromaticNotes[j]);
         const optionEl = document.createElement('option');
         optionEl.value = chromaticNotes[j].value;
         optionEl.innerText = chromaticNotes[j].name;
@@ -110,4 +132,18 @@ function createChromaticDropdown(tuning) {
     }
     return chordNote;
 }
-//assign numbers to every fret
+
+function updateSelectedNotes() {
+    var chordBank = document.getElementById('chordBank')
+    selectedNotes = []
+ //loop through the children of chordbank and find any selected notes.
+    for (i = 0; i < chordBank.children.length; i++){
+        var selectEl = chordBank.children[i]
+        if (selectEl.selectedIndex > 0) {
+            if ( selectedNotes.indexOf(selectEl.selectedIndex) < 0) {
+                selectedNotes.push(selectEl.selectedIndex)
+            }
+        }
+    }            
+    renderFretboard(standardTuning, selectedNotes)
+}
